d81101d50e4c08fce11a5a7a57bae3fec3c63057
Kobus van Schoor <pbscube@gmail.com>
2016-01-09 16:16:25 +0200

    Added testing tools

---
+++ tools.cpp
@@ -0,0 +1,26 @@
+//This is an easy way to test the dchain encryption library
+//^C will exit out of the program
+#include "include/dchain.h"
+#include <iostream>
+#include <limits>
+
+int main() {
+	std::string plaintext, keyword, temp;
+	int count;
+	while (true) {
+		std::cout << "Insert plaintext: ";
+		std::getline(std::cin, plaintext);
+		std::cout << "Insert keyword: ";
+		std::getline(std::cin, keyword);
+		std::cout << "Insert times encryption/decryption should occur: ";
+		std::cin >> count;
+
+		for (int i = 0; i < count; i++) {
+			temp = dchain::strEncrypt(plaintext, keyword);
+			std::cout << "String encrypted: " << temp << '\n';
+		//	std::cout << "String decrypted: " << dchain::strDecrypt(temp, keyword) << '\n';
+		}
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+}
