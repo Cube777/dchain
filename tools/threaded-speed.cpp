fc286d09ce427875ee9f195b9d1b9035f18e1372
Kobus van Schoor <pbscube@gmail.com>
2016-01-23 13:23:40 +0200

    Improved threaded testing tool

--- tools/threaded-speed.cpp
+++ tools/threaded-speed.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <thread>
+#include <chrono>
 
 #define THREADS 4
 #define PLENGTH 1000
@@ -23,36 +24,32 @@ int main()
 	unsigned char* bin = new unsigned char[size];
 	ind.read((char *)bin, size);
 	ind.close();
-	std::string temp;
+	std::string key;
 	for (int i = 0; i < PLENGTH; i++)
-	temp += char(rand() % 94 + 32);
-	double etotal, dtotal;
+		key += char(rand() % 94 + 32);
+	double etotal, dtotal, temp;
 	etotal = 0;
 	dtotal = 0;
-	int active_threads;
-	if (THREADS < std::thread::hardware_concurrency())
-		active_threads = THREADS;
-	else
-		active_threads = std::thread::hardware_concurrency();
 	std::cout <<"All speeds are printed as MB/s...total (per thread)\n";
-	dchain::data dec;
-	for (int i = 0; i < 1; i++) {
-		double start = clock();
-		dchain::data enc = dchain::binThreadEncrypt(bin, size, temp, THREADS);
-		double end = clock();
-		etotal += size / 1000000 / ((end - start) / CLOCKS_PER_SEC / active_threads);
+	std::chrono::high_resolution_clock::time_point t1, t2;
+	std::chrono::duration<double> time;
+	for (int i = 0; i < 100; i++) {
+		t1 = std::chrono::high_resolution_clock::now();
+		dchain::data enc = dchain::binThreadEncrypt(bin, size, key, THREADS);
+		t2 = std::chrono::high_resolution_clock::now();
+		temp = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
+		etotal += size / 1000000 / temp;
 		std::cout << "Encrypt: " << etotal / (i + 1)  << " (" << etotal / (i + 1) / THREADS << ")" << '\t';
 
-		start = clock();
-		dec = dchain::binThreadDecrypt(enc.addr, enc.size, temp, THREADS);
-		end = clock();
-		dtotal += size / 1000000 / ((end - start) / CLOCKS_PER_SEC / active_threads);
+		t1 = std::chrono::high_resolution_clock::now();
+		dchain::data dec = dchain::binThreadDecrypt(enc.addr, enc.size, key, THREADS);
+		t2 = std::chrono::high_resolution_clock::now();
+		temp = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
+		dtotal += size / 1000000 / temp;
 		std::cout << "Decrypt: " << dtotal / (i + 1) << " (" << dtotal / (i + 1) / THREADS << ")"<< '\n';
 		delete[] enc.addr;
+		delete[] dec.addr;
 	}
-	std::ofstream asd("testout", std::ios::binary);
-	asd.write((char *)dec.addr, dec.size);
-	asd.close();
 	delete[] bin;
 	return 0;
 }
